85ed3666afd2302aa0a81a88f2454e0fce63ff89
Omolade Sunday <38852467+Zechariah-dev@users.noreply.github.com>
2024-01-10 21:47:51 +0100

    fix(exception): setup exception module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ExceptionModule } from './infrastructure/exceptions/exceptions.module';
+import { LoggerModule } from './infrastructure/logger/logger.module';
 
 @Module({
-  imports: [],
+  imports: [ExceptionModule, LoggerModule],
   providers: [],
 })
 export class AppModule {}
--- src/infrastructure/exceptions/exceptions.module.ts
+++ src/infrastructure/exceptions/exceptions.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ExceptionsService } from './exceptions.service';
 
-@Module({})
+@Module({ providers: [ExceptionsService], exports: [ExceptionsService] })
 export class ExceptionModule {}
