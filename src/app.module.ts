854aeffacb7a9c183e574590a643fd0c88215cee
Omolade Sunday <38852467+Zechariah-dev@users.noreply.github.com>
2024-01-11 20:00:30 +0100

    feat(prisma): setup prisma database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ExceptionModule } from './infrastructure/exceptions/exceptions.module';
 import { LoggerModule } from './infrastructure/logger/logger.module';
+import { PrismaModule } from './infrastructure/prisma/prisma.module';
 
 @Module({
-  imports: [ExceptionModule, LoggerModule],
+  imports: [ExceptionModule, LoggerModule, PrismaModule],
   providers: [],
 })
 export class AppModule {}
